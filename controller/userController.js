efbb820707c59e5e1f5b7742b9abdd425efd385f
nattadev <fasterkaiy123@gmail.com>
2022-07-21 01:16:52 +0700

    update login

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const UserModel = require('../model/User')
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.index = async (req, res) => {
     try {
@@ -10,19 +12,70 @@ exports.index = async (req, res) => {
     }
   }
 
-  exports.register = async (req,res,next) => {
+  exports.register = async (req,res) => {
     try {
-        const {name,email,password} = req.body
+        const { name, email, password } = req.body;
 
-        let user = new UserModel()
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+
+        const existEmail = await UserModel.findOne({email : email})
+        if(existEmail){
+            return  res.status(400).json({
+                message: 'อีเมลซ้ำ'
+            })
+        }
+
+        let user = new UserModel();
         user.name = name
-        user.email = email
-        user.password = password
+        user.email = email;
+        user.password = await user.encryptPassword(password) ;
 
        await user.save()
 
        return  res.status(201).json({
         message: 'ลงทะเบียนเรียบร้อย'
+    });  
+         
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+  }
+
+
+  exports.login = async (req,res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userCheck = await UserModel.findOne({email : email})
+        if(!userCheck){
+            return  res.status(400).json({
+                message: 'ไม่มีผู้ใช้งานในระบบ'
+            })
+        }
+
+        const isValid = await userCheck.checkPassword(password);
+        if (!isValid) {
+            return  res.status(400).json({
+                message: 'รหัสผ่านไม่ถูกต้อง'
+            })
+        }
+
+        const token = await jwt.sign({
+            id: userCheck.id
+        },'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJ',{expiresIn: '1 day'})
+
+        const expires_in = jwt.decode(token)
+
+       return  res.status(201).json({
+        access_token : token ,
+        expiresIn : expires_in.exp,
+        token_type: 'Bearer',
+        message: 'ล็อกอินสำเร็จ'
     });  
          
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 
 const userController = require('../controller/userController')
 
+//http://localhost:5050/user
 router.get('/', userController.index);
 
-router.post('/register',userController.register);
+//http://localhost:5050/login
+router.post('/login',userController.login)
+
+//http://localhost:5050/register
+router.post('/register',[
+  body('name').not().isEmpty().withMessage('กรุณาป้อนข้อมูลชื่อด้วย'),
+  body('email').not().isEmpty().withMessage('กรุณากรอกอีเมล์ด้วย').isEmail().withMessage('รูปแบบอีเมล์ไม่ถูกต้อง'),
+  body('password').not().isEmpty().withMessage('กรุณากรอกรหัสผ่านด้วย').isLength({min: 3}).withMessage('รหัสผ่านต้อง 3 ตัวอักษรขึ้นไป'),
+]
+,userController.register);
 
 module.exports = router
