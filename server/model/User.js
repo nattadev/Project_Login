7651a30da49325308866c950d7e7a8ea95255e94
Krisanathep Boonpromma <krisanathep.b@gli.co.th>
2022-07-21 10:09:58 +0700

    again

---
+++ server/model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const bcrypt = require('bcrypt')
+
+const schema = new mongoose.Schema({
+    name:  { type: String ,required: true, trim: true},
+    email: { type: String, required: true, trim: true, unique: true, index: true },
+    password: { type: String, required: true, trim: true , minlength: 3 },
+    role: { type: String, default: 'member' }
+  },{
+    collection: 'users'
+  });
+
+  schema.methods.encryptPassword = async function(password) {
+    const salt = await bcrypt.genSalt(5);
+    const hashPassword = await bcrypt.hash(password, salt);
+    return hashPassword;
+ }
+ schema.methods.checkPassword = async function(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+    return isValid;
+ }
+
+
+
+
+
+  const UserModel = mongoose.model('User', schema);
+module.exports = UserModel
